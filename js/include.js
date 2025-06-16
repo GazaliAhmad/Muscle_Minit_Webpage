3474725b4c13ee7b2509aa84aff69b95b7a9a14e
Gazali Ahmad <gazali.ahmad@gmail.com>
2025-06-16 18:37:16 +0800

    Background and Hero
    Hero image and background

---
+++ js/include.js
@@ -0,0 +1,12 @@
+// js/include.js
+document.addEventListener("DOMContentLoaded", function () {
+  const includes = document.querySelectorAll('[data-include]');
+
+  includes.forEach(el => {
+    const file = el.getAttribute('data-include');
+    fetch(file)
+      .then(res => res.text())
+      .then(html => el.innerHTML = html)
+      .catch(err => el.innerHTML = `<p>Error loading ${file}</p>`);
+  });
+});
