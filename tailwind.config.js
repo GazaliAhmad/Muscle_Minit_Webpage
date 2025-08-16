7696b387ab5656689052957136e60c7ff4b958c6
Gazali Ahmad <gazali.ahmad@gmail.com>
2025-08-16 22:09:06 +0800

    index update
    updated index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ["./index.html", "./sections/**/*.html"],
+  content: [
+    "./index.html",
+    "./css/**/*.css",
+    "./**/*.html",
+    "./**/*.js"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
